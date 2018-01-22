123de36716421c3a13b6bb187348a238ae9a6572
VU LAM CAO <vlcao@uow.edu.au>
2018-01-22 13:11:08 +0700

    Add files via upload

---
+++ PythonOptimizationProjects/HappyForex_EA/scr/LogModule/happyforex_Log.py
@@ -0,0 +1,31 @@
+'''
+Created on Dec 27, 2017
+
+@author: cao.vu.lam
+'''
+
+
+#===============================================================================
+# def initialize_logger(file_name):
+#     
+#     logger = logging.getLogger()
+#     logger.setLevel(logging.DEBUG)
+#     
+#     # create a logging format
+#     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#      
+#     # create a file handler
+#     handler_file = logging.FileHandler(file_name)
+#     handler_file.setLevel(logging.INFO)
+#     handler_file.setFormatter(formatter)
+#     logger.addHandler(handler_file)
+#     
+#     
+#     # create console handler and set level to info
+#     handler_console = logging.StreamHandler()
+#     handler_console.setLevel(logging.INFO)
+#     handler_console.setFormatter(formatter)
+#     logger.addHandler(handler_console)
+#===============================================================================
+    
+
